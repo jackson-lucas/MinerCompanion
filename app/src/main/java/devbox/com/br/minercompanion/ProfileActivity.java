580f7f5ea80ad0bea0041d57a33f052d62c7e269
Jackson Lucas <jackson7br@gmail.com>
2015-04-26 03:08:19 -0400

    Basics additions to create connection with server

--- app/src/main/java/devbox/com/br/minercompanion/ProfileActivity.java
+++ app/src/main/java/devbox/com/br/minercompanion/ProfileActivity.java
@@ -1,9 +1,17 @@
 package devbox.com.br.minercompanion;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import devbox.com.br.minercompanion.R;
 
@@ -15,7 +23,6 @@ public class ProfileActivity extends ActionBarActivity {
         setContentView(R.layout.activity_profile);
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,5 +44,39 @@ public class ProfileActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createAlertDialog() {
+        final EditText editText = new EditText(this);
+        editText.setId(R.id.editText);
+        editText.setText("Você está bem?");
+
+
+
+        AlertDialog.Builder observacaoDialog = new AlertDialog.Builder(this);
+        observacaoDialog.setTitle("Alerta")
+                .setView(editText)
+                // Send to answer to server
+                .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                })
+                .create()
+                .show();
+    }
+
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
     }
 }
